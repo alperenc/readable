4c9067c6c57743969b17cfaeb1735462d1bee489
Alp Eren Can <alperenc@gmail.com>
2017-12-08 16:07:00 +0300

    Format with Prettier

--- frontend/src/actions/comments.js
+++ frontend/src/actions/comments.js
@@ -1,32 +1,32 @@
 import {
-    COMMENT_POST,
-    UPVOTE_COMMENT,
-    DOWNVOTE_COMMENT,
-    EDIT_COMMENT,
-    DELETE_COMMENT
-} from './actionTypes'
+  COMMENT_POST,
+  UPVOTE_COMMENT,
+  DOWNVOTE_COMMENT,
+  EDIT_COMMENT,
+  DELETE_COMMENT
+} from "./actionTypes";
 
 export const commentPost = comment => ({
-    type: COMMENT_POST,
-    comment,
-})
+  type: COMMENT_POST,
+  comment
+});
 
 export const upvoteComment = id => ({
-    type: UPVOTE_COMMENT,
-    id,
-})
+  type: UPVOTE_COMMENT,
+  id
+});
 
 export const downvoteComment = id => ({
-    type: DOWNVOTE_COMMENT,
-    id,
-})
+  type: DOWNVOTE_COMMENT,
+  id
+});
 
 export const editComment = (id, body) => ({
-    type: EDIT_COMMENT,
-    comment,
-})
+  type: EDIT_COMMENT,
+  comment
+});
 
 export const deleteComment = id => ({
-    type: DELETE_COMMENT,
-    id,
-})
+  type: DELETE_COMMENT,
+  id
+});
